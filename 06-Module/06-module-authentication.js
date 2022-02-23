3e4c56ca9084730272188c0881838f6df9772c98
Rizkiagunawan <98855192+Rizkiagunawan@users.noreply.github.com>
2022-02-23 21:44:57 +0800

    Update 06-module-authentication.js

--- 06-Module/06-module-authentication.js
+++ 06-Module/06-module-authentication.js
@@ -1,15 +1,13 @@
-export default function validateLoginData(email, password) {
-    const emailValidator = new RegExp( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-    const passwordValidator = new RegExp(
-    "^(?=.[A-Za-z])(?=.\d)(?=.[@$!%#?&])[A-Za-z\d@$!%*#?&]{8,}$"
-    );
-    if (emailValidator.test(email)) {
-    if (passwordValidator.test(password)) {
-        return `Welcome to The Jungle`;
-    } else {
-        return `tolong masukan password sesuai dengan ketentuan`;
-    }
-    }
-    return `tolong masukan email yang valid`;
+export default function validateLoginData(email, password){
+    const emailValidator = new RegExp(/^\S+@\S+\.\S+$/);
+    const passwordValidator = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")
+      if (emailValidator.test(email)) {
+        if(passwordValidator.test(password)){
+          return `Welcome to The Jungle`;
+        } else {
+          return `tolong masukan password sesuai dengan ketentuan`;
+        }
+      }
+     return `tolong masukan email yang valid`;
+
 }
