5bd24907e9fccd416e4fd62cc8ba27b55551291a
Rizkiagunawan <98855192+Rizkiagunawan@users.noreply.github.com>
2022-02-23 21:43:38 +0800

    Update 06-module-helpers.js

--- 06-Module/06-module-helpers.js
+++ 06-Module/06-module-helpers.js
@@ -1,17 +1,9 @@
-function sayHello() {
-    console.log("Hello");
+// soal - 01
+
+export default function sayHallo(){
+    console.log("Hello")
 }
 
-    export default sayHello();
-
-
-    const elFormGreeting = document.querySelector("#form-greeting");
-    const btn = elFormGreeting.lastElementChild
-    const elInputName = document.querySelector("#input-name");
-
-    btn.onclick = greeting
-
-    export function greeting (){
-    var message = elInputName.value
-    alert(`Hello, ${message}`)
+export function greeting(name){
+    alert(`Hello  ${name}`);
 }
